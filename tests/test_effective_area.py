828692c3da1ef87b49fd50aec6a956a449cc6e70
Robert Stein <robert.stein@desy.de>
2020-02-18 10:02:27 +0100

    Update test.

--- tests/test_effective_area.py
+++ tests/test_effective_area.py
@@ -31,21 +31,45 @@ class TestTimeIntegrated(unittest.TestCase):
                                     f"as this has not yet been implemented")
                     pass
 
+            # Test ValueError for string declinations
+
+            try:
+                get_aeff(declination_deg="7", energy_pdf=epdf, selection=selection)
+            except ValueError:
+                pass
+
+        # Test ValueError for incorrect selections
+
+        try:
+            get_aeff(declination_deg=0, energy_pdf=epdf, selection="something_else")
+        except ValueError:
+            pass
+
+    def test_get_nexp(self):
+
+        epdf = EnergyPDF.create({
+            "energy_pdf_name": "power_law",
+            "gamma": 2.0,
+            "e_min_gev": 2 * 10. ** 5,
+            "e_max_gev": 10. ** 7
+        })
+
         # res = power_law_aeff(declination_deg=10.)
         #
         # divide_time(res, 0.5 * u.year)
         #
-        # norm_energy = 1. * u.PeV
-        #
-        # txs_value_100tev = 2. * 0.5 * u.year * 1.6e-15 / u.TeV / u.cm ** 2 / u.s
-        # txs_value_at_norm = (txs_value_100tev * (norm_energy / (100 * u.TeV)) ** -2.).to("GeV^-1 cm^-2")
-        #
-        # # get_threshold_flux(declination_deg=10., energy_pdf=epdf, norm_energy=norm_energy)
-        #
-        # get_nexp(txs_value_at_norm, declination_deg=10., energy_pdf=epdf, norm_energy=norm_energy,
-        #          selection="alerts_v1")
-        #
-        # get_e2dnde(txs_value_100tev / (0.5 * u.year), 100 * u.TeV)
+        norm_energy = 1. * u.PeV
+
+        # Taken from https://arxiv.org/abs/1807.08816 (Fig 4)
+        txs_value_e2dnde = 0.5 * u.year * 5.5 * 10**-11 * u.erg / u.cm**2 / u.s
+        txs_value_at_norm = (txs_value_e2dnde / (norm_energy**2.)).to("GeV^-1 cm^-2")
+
+        n_exp = get_nexp(txs_value_at_norm, declination_deg=10., energy_pdf=epdf, norm_energy=norm_energy,
+                 selection="alerts_v1").to("").value
+
+        diff = abs(1. - n_exp)
+
+        self.assertAlmostEqual(diff, 0., places=1)
 
 
 if __name__ == '__main__':
